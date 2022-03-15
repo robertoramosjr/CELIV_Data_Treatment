71c393feca5b2abb305acfd57950e7274ecd1e09
robertoramosjr <robeerto.aguiar.ramos@gmail.com>
2022-03-15 18:24:45 -0300

    4 - function to find the peaks added and validated

--- project_script/datatreatment.py
+++ project_script/datatreatment.py
@@ -1,4 +1,6 @@
 import os.path
+import numpy as np
+from scipy.signal import savgol_filter
 
 
 def separate_odd_columns(data_frame):
@@ -19,3 +21,35 @@ def is_bigger_data_frame(data_frame_1, data_frame_2):
 
 def equalize_data_frame_rows(data_frame_1, data_frame_2):
     return data_frame_1.loc[0:len(data_frame_2)-1, :]
+
+
+def smooth(array):
+    temp_list = []
+    for key, values in enumerate(array):
+        temp_list.append((savgol_filter(array[key], 55, 2).tolist()))
+    return temp_list
+
+
+def find_the_peaks(data_as_list):
+    temp_list = []
+    for key, value in enumerate(data_as_list):
+        temp_list.append(data_as_list[key].max())
+    return temp_list
+
+
+def find_peak_index(array, peak_list):
+    temp_list = []
+    for key, values in enumerate(array):
+        temp_list.append(np.where(array[key] == peak_list[key]))
+    return temp_list
+
+
+def find_time_max(array, index_list):
+    temp_list = []
+    for key, value in enumerate(array):
+        temp_list.append(array[key][index_list[key]].tolist())
+    return temp_list
+
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
--- project_script/main.py
+++ project_script/main.py
@@ -4,6 +4,7 @@ from scipy import integrate
 import ask
 import datatreatment as dt
 import constantvalues as cv
+import numpy as np
 
 
 def message_invalid_path():
@@ -32,8 +33,8 @@ def integrate_data():
     return integrated_values
 
 
-# device_thickness = ask.device_thickness()
-# device_area = ask.device_area()
+# DEVICE_THICKNESS = ask.device_thickness()
+# DEVICE_AREA = ask.device_area()
 CHARGE_DENSITY_CALCULATION = cv.CURRENT_CORRECTION_FACTOR * cv.CHARGE_DENSITY_CORRECTION_FACTOR \
                                       / (cv.ELECTRON_CHARGE * cv.DEVICE_AREA * cv.DEVICE_THICKNESS)
 
@@ -82,19 +83,16 @@ odd_columns_subtracted = current_photo_celiv_ramp_time\
 
 sanitize_data_frames()
 
-data_ramp_time = pd.concat(
-        [time_photo_celiv_ramp_time, odd_columns_subtracted],
-        axis=1
-    )\
+data_ramp_time = pd.concat([time_photo_celiv_ramp_time, odd_columns_subtracted], axis=1)\
     .sort_index(axis=1)
 
 # data_ramp_time.to_excel("C:/Users/robee/Desktop/data_ramp_time.xlsx")
 
-data_ramp_time.to_csv(ask.file_name(), sep='\t')     # line commented to run tests
+# data_ramp_time.to_csv(ask.file_name(), sep='\t')     # line commented to run tests
 
 data_ramp_time_transposed_in_arrays = data_ramp_time.transpose().to_numpy()
 
-time_photo_celiv_ramp_time_transposed = time_photo_celiv_ramp_time.transpose().to_numpy()
+time_photo_celiv_ramp_time_transposed = time_photo_celiv_ramp_time.dropna().transpose().to_numpy()
 
 odd_columns_subtracted_transposed_in_arrays = odd_columns_subtracted.transpose().to_numpy()
 
@@ -104,8 +102,49 @@ integration_results = integrate_data()
 
 density_of_carriers = [element * CHARGE_DENSITY_CALCULATION for element in integration_results]
 
-pd.DataFrame(density_of_carriers).to_csv(
-    "C:/Users/robee/Desktop/density_of_carriers_test.txt",
-    sep='\t',
-    decimal=','
-    )
+# pd.DataFrame(density_of_carriers).to_csv(
+#     "C:/Users/robee/Desktop/density_of_carriers_test.txt",
+#     sep='\t',
+#     decimal=',',
+#     )
+
+odd_columns_subtracted_smoothed_transposed = np.array(dt.smooth(odd_columns_subtracted_transposed_in_arrays))
+
+peaks = dt.find_the_peaks(odd_columns_subtracted_smoothed_transposed)
+
+indexes_list = dt.find_peak_index(odd_columns_subtracted_smoothed_transposed, peaks)
+
+time_max_values = dt.flatten(dt.find_time_max(time_photo_celiv_ramp_time_transposed, indexes_list))
+
+peak_values = pd.DataFrame([peaks, time_max_values]).transpose().set_axis(['current peak', 'peak time'], axis=1)
+
+peak_values['first term of mobility calculations'] = cv.DEVICE_THICKNESS ** 2 / (2 * peak_values['peak time'] ** 2) \
+    .round(decimals=30)
+
+
+# for k, v in enumerate(odd_columns_subtracted_smoothed_transposed):
+#     plt.plot(time_photo_celiv_ramp_time_transposed[k][:], odd_columns_subtracted_smoothed_transposed[k][:])
+# plt.xlabel('Time (\u03BC s)')
+# plt.ylabel('\u0394 i (mA)')
+# for k, v in enumerate(peaks):
+#     plt.scatter(time_max_values[k], peaks[k])
+#
+# plt.show()
+
+
+
+"""
+----------------Plot used to validate the script that finds the peaks---------------------------------------------------
+----------------A sampling of 77 measurements were used-----------------------------------------------------------------
+----------------The curves were ploted superposed (raw and smoothed data) and compared one by one with------------------ 
+----------------the peaks highlighted. With this, I could check the position of the peak found by the script------------
+----------------(smoothed data) with the position I would determine visually. The results fits well---------------------
+----------------with a maximum difference of 0.2 us.--------------------------------------------------------------------
+for k, v in enumerate(odd_columns_subtracted_smoothed_transposed):
+    plt.plot(time_photo_celiv_ramp_time_transposed[k][:], odd_columns_subtracted_smoothed_transposed[k][:])
+    plt.plot(time_photo_celiv_ramp_time_transposed[k][:], odd_columns_subtracted_transposed_in_arrays[k][:], 'r')
+    plt.xlabel('Time (\u03BC s)')
+    plt.ylabel('\u0394 i (mA)')
+    plt.scatter(time_max_values[k], peaks[k])
+    plt.show()
+"""
